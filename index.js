cac0b66c35b186ef4595bec24b06c1cdca324df4
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-03-06 21:39:31 +0100

    configure .env variable for client url

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_REQUEST_URL,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello from payment server");
