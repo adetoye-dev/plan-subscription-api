0948566a2656b702d06b664becc003c62b85058f
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-03-08 21:58:56 +0100

    create request handler to initialize payment

--- payment.js
+++ payment.js
@@ -1,9 +1,12 @@
 import express from "express";
+import { initializePayment } from "./handlers/initialize.js";
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.send("hello from payment route");
 });
+
+router.post("/initialize", initializePayment);
 
 export default router;
