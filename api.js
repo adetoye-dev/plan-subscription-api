2a837514d4980bce6938c35c36ac218adf02b90c
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-03-09 15:42:08 +0100

    Setup header configuration

--- api.js
+++ api.js
@@ -1,7 +1,14 @@
 import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const api = axios.create({
   baseURL: process.env.BASE_URL,
+  headers: {
+    Authorization: "Bearer " + process.env.BEARER_SECRET_KEY,
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
