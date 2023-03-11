267003bc34dc1d42d1d403a266229b1199566f47
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-03-11 18:08:31 +0100

    Redirect to thank-you page on payment success

--- handlers/verify.js
+++ handlers/verify.js
@@ -4,11 +4,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const verifyPayment = async (req, res) => {
+  const clientUrl = process.env.CLIENT_URL;
   console.log("verification api was called");
   console.log(req.params);
   const ref = req.params.reference;
   try {
-    const data = await api.get("/transaction/verify/" + ref);
+    const { data } = await api.get("/transaction/verify/" + ref);
+    if (data.data.status === "success")
+      return res.status(200).redirect(clientUrl + "/thank-you");
     console.log(data.data);
     res.json(data.data);
   } catch (error) {
