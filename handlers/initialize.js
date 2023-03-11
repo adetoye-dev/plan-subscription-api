80b84e408f7e6097d4f6b970e17df0201e77740d
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-03-11 17:34:58 +0100

    Setup payment initialization handler

--- handlers/initialize.js
+++ handlers/initialize.js
@@ -1,3 +1,22 @@
-export const initializePayment = (req, res) => {
-  res.send("initialize called");
+import api from "../api.js";
+import dotenv from "dotenv";
+import { nanoid } from "nanoid";
+
+dotenv.config();
+
+export const initializePayment = async (req, res) => {
+  const ref = nanoid();
+  try {
+    const data = await api.post("/transaction/initialize", {
+      amount: "1000",
+      email: req.body.email,
+      reference: ref,
+      callback_url: process.env.SERVER_URL + "/api/payment/verify/" + ref,
+      channels: ["card"],
+    });
+    console.log(data.data);
+    res.json(data.data);
+  } catch (error) {
+    res.json(error.data);
+  }
 };
